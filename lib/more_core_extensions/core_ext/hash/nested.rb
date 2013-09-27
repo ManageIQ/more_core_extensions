a392c6119a44ad806416f6a70596fed0c96f528a
Brandon Dunne <bdunne@redhat.com>
2013-09-27 16:58:28 -0400

    Move #has_key_path? for usage on anything that responds to #has_key_path?

--- lib/more_core_extensions/core_ext/hash/nested.rb
+++ lib/more_core_extensions/core_ext/hash/nested.rb
@@ -5,22 +5,6 @@ module MoreCoreExtensions
     include MoreCoreExtensions::Shared::Nested
     extend  MoreCoreExtensions::Shared::Nested
 
-    def has_key_path?(*args)
-      args = args.first if args.length == 1 && args.first.kind_of?(Array)
-      raise ArgumentError, "must pass at least one key" if args.empty?
-
-      key = args.first
-      has_child = self.has_key?(key)
-      return has_child if args.length == 1
-
-      child = self[key]
-      return false unless child.kind_of?(Hash)
-      return child.has_key_path?(args[1..-1])
-    end
-    alias include_path? has_key_path?
-    alias key_path?     has_key_path?
-    alias member_path?  has_key_path?
-
     def delete_path(*args)
       args = args.first if args.length == 1 && args.first.kind_of?(Array)
       raise ArgumentError, "must pass at least one key" if args.empty?
--- lib/more_core_extensions/core_ext/shared/nested.rb
+++ lib/more_core_extensions/core_ext/shared/nested.rb
@@ -14,6 +14,24 @@ module MoreCoreExtensions
         return child.fetch_path(args[1..-1])
       end
 
+      def has_key_path?(*args)
+        args = args.first if args.length == 1 && args.first.kind_of?(Array)
+        raise ArgumentError, "must pass at least one key" if args.empty?
+
+        key = args.first
+        raise ArgumentError, "must be a number" if self.kind_of?(Array) && !key.kind_of?(Numeric)
+
+        has_child = self.kind_of?(Array) ? (self.length > 0 && (self.length.succ * -1).upto(self.length).include?(key)) : self.has_key?(key)
+        return has_child if args.length == 1
+
+        child = self[key]
+        return false unless child.respond_to?(:has_key_path?)
+        return child.has_key_path?(args[1..-1])
+      end
+      alias include_path? has_key_path?
+      alias key_path?     has_key_path?
+      alias member_path?  has_key_path?
+
       def store_path(*args)
         raise ArgumentError, "must pass at least one key, and a value" if args.length < 2
         value = args.pop
