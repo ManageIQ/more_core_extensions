d84c71cda237d98263538b5ef1a35982f81f4533
Joe Rafaniello <jrafanie@redhat.com>
2014-11-06 16:30:44 -0500

    Use transpec to migrate to rspec 3.

--- spec/core_ext/array/deletes_spec.rb
+++ spec/core_ext/array/deletes_spec.rb
@@ -2,16 +2,16 @@ require_relative "../../spec_helper"
 
 describe Array do
   it "#delete_nils" do
-    [].delete_nils.should == []
-    [1].delete_nils.should == [1]
-    [nil].delete_nils.should == []
-    [1, [], nil].delete_nils.should == [1, []]
+    expect([].delete_nils).to eq([])
+    expect([1].delete_nils).to eq([1])
+    expect([nil].delete_nils).to eq([])
+    expect([1, [], nil].delete_nils).to eq([1, []])
   end
 
   it "#delete_blanks" do
-    [].delete_blanks.should == []
-    [1].delete_blanks.should == [1]
-    [nil].delete_blanks.should == []
-    [1, [], nil].delete_blanks.should == [1]
+    expect([].delete_blanks).to eq([])
+    expect([1].delete_blanks).to eq([1])
+    expect([nil].delete_blanks).to eq([])
+    expect([1, [], nil].delete_blanks).to eq([1])
   end
 end
--- spec/core_ext/string/formats_spec.rb
+++ spec/core_ext/string/formats_spec.rb
@@ -2,75 +2,75 @@ require_relative "../../spec_helper"
 
 describe String do
   it '#email?' do
-    "john@example.com".should be_email
-    "john.doe@example.com".should be_email
+    expect("john@example.com").to be_email
+    expect("john.doe@example.com").to be_email
 
-    "john.doe@examplecom".should_not be_email
-    "john.doe@example-com".should_not be_email
-    "".should_not be_email
-    "foo".should_not be_email
+    expect("john.doe@examplecom").not_to be_email
+    expect("john.doe@example-com").not_to be_email
+    expect("").not_to be_email
+    expect("foo").not_to be_email
   end
 
   it '#domain_name?' do
-    "example.com".should be_domain_name
+    expect("example.com").to be_domain_name
 
-    "example..com".should_not be_domain_name
-    "john.doe@example.com".should_not be_domain_name
-    "".should_not be_domain_name
-    "foo".should_not be_domain_name
+    expect("example..com").not_to be_domain_name
+    expect("john.doe@example.com").not_to be_domain_name
+    expect("").not_to be_domain_name
+    expect("foo").not_to be_domain_name
   end
 
   it '#ipv4?' do
-    "192.168.252.15".should be_ipv4
+    expect("192.168.252.15").to be_ipv4
 
-    "392.168.252.15".should_not be_ipv4
-    "".should_not be_ipv4
-    "foo".should_not be_ipv4
-    "::1".should_not be_ipv4                            # 127.0.0.1 in IPv6
-    "1762:0:0:0:0:B03:1:AF18".should_not be_ipv4        # Standard Notation
-    "1762:0:0:0:0:B03:127.32.67.15".should_not be_ipv4  # Mixed Notation
-    "1762::B03:1:AF18".should_not be_ipv4               # Compressed Notation
+    expect("392.168.252.15").not_to be_ipv4
+    expect("").not_to be_ipv4
+    expect("foo").not_to be_ipv4
+    expect("::1").not_to be_ipv4                            # 127.0.0.1 in IPv6
+    expect("1762:0:0:0:0:B03:1:AF18").not_to be_ipv4        # Standard Notation
+    expect("1762:0:0:0:0:B03:127.32.67.15").not_to be_ipv4  # Mixed Notation
+    expect("1762::B03:1:AF18").not_to be_ipv4               # Compressed Notation
   end
 
   it '#ipv6?' do
-    "::1".should be_ipv6                            # 127.0.0.1 in IPv6
-    "1762:0:0:0:0:B03:1:AF18".should be_ipv6        # Standard Notation
-    "1762:0:0:0:0:B03:127.32.67.15".should be_ipv6  # Mixed Notation
-    "1762::B03:1:AF18".should be_ipv6               # Compressed Notation
+    expect("::1").to be_ipv6                            # 127.0.0.1 in IPv6
+    expect("1762:0:0:0:0:B03:1:AF18").to be_ipv6        # Standard Notation
+    expect("1762:0:0:0:0:B03:127.32.67.15").to be_ipv6  # Mixed Notation
+    expect("1762::B03:1:AF18").to be_ipv6               # Compressed Notation
 
-    "192.168.252.15".should_not be_ipv6
-    "392.168.252.15".should_not be_ipv6
-    "".should_not be_ipv6
-    "foo".should_not be_ipv6
+    expect("192.168.252.15").not_to be_ipv6
+    expect("392.168.252.15").not_to be_ipv6
+    expect("").not_to be_ipv6
+    expect("foo").not_to be_ipv6
   end
 
   it "#ipaddress?" do
-    "192.168.252.15".should be_ipaddress
-    "::1".should be_ipaddress                            # 127.0.0.1 in IPv6
-    "1762:0:0:0:0:B03:1:AF18".should be_ipaddress        # Standard Notation
-    "1762:0:0:0:0:B03:127.32.67.15".should be_ipaddress  # Mixed Notation
-    "1762::B03:1:AF18".should be_ipaddress               # Compressed Notation
+    expect("192.168.252.15").to be_ipaddress
+    expect("::1").to be_ipaddress                            # 127.0.0.1 in IPv6
+    expect("1762:0:0:0:0:B03:1:AF18").to be_ipaddress        # Standard Notation
+    expect("1762:0:0:0:0:B03:127.32.67.15").to be_ipaddress  # Mixed Notation
+    expect("1762::B03:1:AF18").to be_ipaddress               # Compressed Notation
 
-    "392.168.252.15".should_not be_ipaddress
-    "".should_not be_ipaddress
-    "foo".should_not be_ipaddress
+    expect("392.168.252.15").not_to be_ipaddress
+    expect("").not_to be_ipaddress
+    expect("foo").not_to be_ipaddress
   end
 
   it "#integer?" do
-    "100".should be_integer
-    "-100".should be_integer
+    expect("100").to be_integer
+    expect("-100").to be_integer
 
-    "".should_not be_integer
-    "100.2".should_not be_integer
-    "A".should_not be_integer
-    "100A".should_not be_integer
+    expect("").not_to be_integer
+    expect("100.2").not_to be_integer
+    expect("A").not_to be_integer
+    expect("100A").not_to be_integer
   end
 
   it '#guid?' do
-    '01234567-89ab-cdef-abcd-ef0123456789'.should be_guid
+    expect('01234567-89ab-cdef-abcd-ef0123456789').to be_guid
 
-    '012ZZZ67-89ab-cdef-abcd-ef0123456789'.should_not be_guid
-    "".should_not be_guid
-    "foo".should_not be_guid
+    expect('012ZZZ67-89ab-cdef-abcd-ef0123456789').not_to be_guid
+    expect("").not_to be_guid
+    expect("foo").not_to be_guid
   end
 end
