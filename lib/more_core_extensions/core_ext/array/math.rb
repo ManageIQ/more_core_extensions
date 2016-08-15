21724928a67b198246e4b59d258b9c4a2b0e2459
Brandon Dunne <bdunne@redhat.com>
2016-08-15 14:40:51 -0400

    Array#variance should return 0.0 for arrays with fewer than 2 items

--- lib/more_core_extensions/core_ext/array/math.rb
+++ lib/more_core_extensions/core_ext/array/math.rb
@@ -27,6 +27,8 @@ module MoreCoreExtensions
     #   [1, 2, 3, 4, 5].variance  #=> 2.5
     #   [1.0, 2.0, 3.0].variance  #=> 1.0
     def variance
+      divisor = length - 1
+      return 0.0 unless divisor > 0
       μ = mean
       reduce(0) { |m, i| m + (i - μ).square } / (length - 1)
     end
