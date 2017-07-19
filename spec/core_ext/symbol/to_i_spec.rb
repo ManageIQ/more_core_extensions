951dbc3f87a83e77050e4fb2e14508592c5b2594
Brandon Dunne <bdunne@redhat.com>
2017-07-19 13:30:25 -0400

    Extract Symbol#to_i from manageiq-gems-pending

---
+++ spec/core_ext/symbol/to_i_spec.rb
@@ -0,0 +1,9 @@
+describe Symbol do
+  it "#to_i" do
+    expect(:"1".to_i).to eq(1)
+    expect(:"-1".to_i).to eq(-1)
+    expect(:test.to_i).to eq(0)
+    expect(:test1.to_i).to eq(0)
+    expect(:"1test".to_i).to eq(1)
+  end
+end
