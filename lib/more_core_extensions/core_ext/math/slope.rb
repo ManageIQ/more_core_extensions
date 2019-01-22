039ba73ad46667d838379c123ba6ef70b64cae6d
Jason Frey <jfrey@redhat.com>
2019-01-22 18:10:58 -0500

    Cleanup code for readability

--- lib/more_core_extensions/core_ext/math/slope.rb
+++ lib/more_core_extensions/core_ext/math/slope.rb
@@ -4,8 +4,9 @@ module MoreCoreExtensions
   module MathSlope
     module ClassMethods
       # Finds the y coordinate given x, slope of the line and the y intercept
+      #
       # `y = mx + b`
-      # Where m is the slope of the line and b is the y intercept (the location where the line crosses the y axis)
+      # Where `m` is the slope of the line and `b` is the y intercept
       #
       #    Math.slope_y_intercept(1, 0.5, 1) # => 1.5
       def slope_y_intercept(x, slope, y_intercept)
@@ -13,44 +14,36 @@ module MoreCoreExtensions
       end
 
       # Finds the x coordinate given y, slope of the line and the y intercept
+      #
       # `x = (y - b) / m`
-      # Where m is the slope of the line and b is the y intercept (the location where the line crosses the y axis)
+      # Where `m` is the slope of the line and `b` is the y intercept
       #
       #    Math.slope_x_intercept(1.5, 0.5, 1) # => 1.0
       def slope_x_intercept(y, slope, y_intercept)
-        (y - y_intercept) / slope
+        (y - y_intercept) / slope.to_f
       end
 
       # Finds the linear regression of the given coordinates.  Coordinates should be given as x, y pairs.
       #
-      # Returns the slope of the line, the y intercept (the location where the line crosses the y axis), and the
-      # R-squared value (how close the data fits to the trend line)
+      # Returns the slope of the line, the y intercept, and the R-squared value.
       #
       #    Math.linear_regression([1.0, 1.0], [2.0, 2.0]) # => [1.0, 0.0, 1.0]
       def linear_regression(*coordinates)
         return if coordinates.empty?
 
-        n = coordinates.length
         x_array, y_array = coordinates.transpose
+        sum_x  = x_array.sum
+        sum_x2 = x_array.map(&:square).sum
+        sum_y  = y_array.sum
+        sum_y2 = y_array.map(&:square).sum
+        sum_xy = coordinates.map { |x, y| x * y }.sum
 
-        sum_x = x_array.sum
-        sum_y = y_array.sum
-
-        xy_array = []
-        n.times { |i| xy_array.push(x_array[i] * y_array[i]) }
-
-        sum_xy = xy_array.sum
-
-        x2_array = x_array.inject([]) { |a, v| a.push(v.square) }
-        y2_array = y_array.inject([]) { |a, v| a.push(v.square) }
-
-        sum_x2 = x2_array.sum
-        sum_y2 = y2_array.sum
-
+        n = coordinates.size.to_f
         slope = (n * sum_xy - sum_x * sum_y) / (n * sum_x2 - sum_x.square)
-        return [] if slope.nan?
+        return if slope.nan?
+
         y_intercept = (sum_y - slope * sum_x) / n
-        r_squared = (n * sum_xy - sum_x * sum_y) / Math.sqrt((n * sum_x2 - sum_x.square) * (n * sum_y2 - sum_y.square)) rescue nil
+        r_squared   = (n * sum_xy - sum_x * sum_y) / Math.sqrt((n * sum_x2 - sum_x.square) * (n * sum_y2 - sum_y.square)) rescue nil
 
         return slope, y_intercept, r_squared
       end
--- spec/core_ext/math/slope_spec.rb
+++ spec/core_ext/math/slope_spec.rb
@@ -4,6 +4,8 @@ describe Math do
     expect(described_class.slope_y_intercept(2, 1, 0)).to eq(2)
     expect(described_class.slope_y_intercept(10, 1, 0)).to eq(10)
     expect(described_class.slope_y_intercept(10, 2, 0)).to eq(20)
+
+    expect(described_class.slope_y_intercept(0.5, 1, 1)).to eq(1.5)
   end
 
   it ".slope_x_intercept" do
@@ -11,9 +13,13 @@ describe Math do
     expect(described_class.slope_x_intercept(2, 1, 0)).to eq(2)
     expect(described_class.slope_x_intercept(10, 1, 0)).to eq(10)
     expect(described_class.slope_x_intercept(10, 2, 0)).to eq(5)
+
+    expect(described_class.slope_x_intercept(1.5, 1, 1)).to eq(0.5)
   end
 
   it ".linear_regression" do
+    expect(described_class.linear_regression([1, 1], [2, 2])).to eq([1.0, 0.0, 1.0])
+
     expect(described_class.linear_regression([1.0, 1.0], [2.0, 2.0])).to eq([1.0, 0.0, 1.0])
     expect(described_class.linear_regression([1.0, 1.0], [1.5, 2.0])).to eq([2.0, -1.0, 1.0])
   end
