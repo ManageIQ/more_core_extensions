5d9d1ddbba0e574c1d6129bdac8f49c1b8745b96
Daniel Berger <djberg96@gmail.com>
2020-06-10 15:41:06 -0400

    Remove comment about providers, change method name to just clean.

--- lib/more_core_extensions/core_ext/digest/uuid.rb
+++ lib/more_core_extensions/core_ext/digest/uuid.rb
@@ -5,8 +5,8 @@ module Digest
     UUID_REGEX_FORMAT = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
 
     # Takes a UUID string of varying formats and cleans it. It will strip invalid characters,
-    # such as leading and trailing brackets as well as whitespace, which some providers insert
-    # into their UUID strings. The result is a lowercased, canonical UUID string.
+    # such as leading and trailing brackets as well as whitespace. The result is a lowercased,
+    # canonical UUID string.
     #
     # If the +guid+ argument is nil or blank, then nil is returned. If the +guid+ is already
     # clean, then no additional cleaning occurs, and it is returned as-is.
@@ -14,7 +14,7 @@ module Digest
     # @param guid [String] A string that should more or less represent a UUID.
     # @return [String] A lowercase v4 UUID string stripped of any extraneous characters.
     #
-    def self.clean_guid(guid)
+    def self.clean(guid)
       return nil if guid.nil?
       g = guid.to_s.downcase
       return nil if g.strip.empty?
--- spec/core_ext/digest/uuid_spec.rb
+++ spec/core_ext/digest/uuid_spec.rb
@@ -1,34 +1,34 @@
 describe Digest::UUID do
-  context "clean_guid" do
+  context "clean" do
     let(:expected) { '01234567-89ab-cdef-abcd-ef0123456789' }
 
     it "handles a valid lowercase guid as expected" do
-      expect(described_class.clean_guid('01234567-89ab-cdef-abcd-ef0123456789')).to eq(expected)
+      expect(described_class.clean('01234567-89ab-cdef-abcd-ef0123456789')).to eq(expected)
     end
 
     it "handles a Microsoft-style guid as expected" do
-      expect(described_class.clean_guid('{01234567-89ab-cdef-abcd-ef0123456789}')).to eq(expected)
+      expect(described_class.clean('{01234567-89ab-cdef-abcd-ef0123456789}')).to eq(expected)
     end
 
     it "handles a valid uppercase guid as expected" do
-      expect(described_class.clean_guid('01234567-89AB-CDEF-abcd-ef0123456789')).to eq(expected)
+      expect(described_class.clean('01234567-89AB-CDEF-abcd-ef0123456789')).to eq(expected)
     end
 
     it "handles a valid guid with an invalid structure as expected" do
-      expect(described_class.clean_guid('01 23 45 67 89 AB CD EF-AB CD EF 01 23 45 67 89')).to eq(expected)
+      expect(described_class.clean('01 23 45 67 89 AB CD EF-AB CD EF 01 23 45 67 89')).to eq(expected)
     end
 
     it "handles an explicit nil or blank string as expected" do
-      expect(described_class.clean_guid(nil)).to be_nil
-      expect(described_class.clean_guid('')).to be_nil
+      expect(described_class.clean(nil)).to be_nil
+      expect(described_class.clean('')).to be_nil
     end
 
     it "handles invalid guids as expected" do
-      expect(described_class.clean_guid('sdkjfLSDLK')).to be_nil
-      expect(described_class.clean_guid('sdkjfLSD-LKJF-8367-41df-FLKD209alkfd')).to be_nil
-      expect(described_class.clean_guid('01234567-89AB-CDEF-abcd-efgggggggggg')).to be_nil
-      expect(described_class.clean_guid('01234567-89AB-CDEF-abcd-ef0123456789a')).to be_nil
-      expect(described_class.clean_guid('01234567-89AB-CDEF-abcd-ef012345678')).to be_nil
+      expect(described_class.clean('sdkjfLSDLK')).to be_nil
+      expect(described_class.clean('sdkjfLSD-LKJF-8367-41df-FLKD209alkfd')).to be_nil
+      expect(described_class.clean('01234567-89AB-CDEF-abcd-efgggggggggg')).to be_nil
+      expect(described_class.clean('01234567-89AB-CDEF-abcd-ef0123456789a')).to be_nil
+      expect(described_class.clean('01234567-89AB-CDEF-abcd-ef012345678')).to be_nil
     end
   end
 end
