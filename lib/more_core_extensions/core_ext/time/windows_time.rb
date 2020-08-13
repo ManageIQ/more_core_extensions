ef8a6e8a49632460f11c57b4c076c6cc56f5687e
Daniel Berger <djberg96@gmail.com>
2020-08-13 13:27:04 -0400

    Rename methods, add new method, add specs.

--- lib/more_core_extensions/core_ext/time/windows_time.rb
+++ lib/more_core_extensions/core_ext/time/windows_time.rb
@@ -1,10 +1,17 @@
 module MoreCoreExtensions
   module WindowsTime
-    # Convert 64-bit FILETIME integer into Time object.
-    def wtime2time(wtime)
-      Time.at((wtime - 116444736000000000) / 10000000)
+    module ClassMethods
+      # Convert 64-bit FILETIME integer into Time object.
+      def wtime_to_time(wtime)
+        Time.utc(1601) + wtime.quo(10_000_000)
+      end
+
+      # Convert Time object or Integer object into 64-bit FILETIME.
+      def time_to_wtime(time)
+        time.gmtime.to_i * 10_000_000 + 116_444_736_000_000_000
+      end
     end
   end
 end
 
-Time.extend(MoreCoreExtensions::WindowsTime)
+Time.send(:extend, MoreCoreExtensions::WindowsTime::ClassMethods)
---
+++ spec/core_ext/time/wtime2time_spec.rb
@@ -0,0 +1,31 @@
+require 'time'
+
+RSpec.describe Time do
+  let(:wtime) { 131172192730000000 }
+  let(:time) { Time.parse("2016-09-01 16:01:13 UTC") }
+
+  context '.wtime_to_time' do
+    it "returns the expected time" do
+      expect(Time.wtime_to_time(wtime).to_s).to eq("2016-09-01 16:01:13 UTC")
+      expect(Time.wtime_to_time(0).to_s).to eq("1601-01-01 00:00:00 UTC")
+    end
+
+    it "handles very large values" do
+      expect(Time.wtime_to_time(9**40).to_s).to eq("468387534430910575002897-09-25 22:10:38 UTC")
+    end
+
+    it "requires a single argument" do
+      expect { Time.wtime_to_time }.to raise_error(ArgumentError)
+    end
+  end
+
+  context '.time_to_wtime' do
+    it "returns the expected time" do
+      expect(Time.time_to_wtime(time)).to eq(wtime)
+    end
+
+    it "requires a single argument" do
+      expect { Time.time_to_wtime }.to raise_error(ArgumentError)
+    end
+  end
+end
